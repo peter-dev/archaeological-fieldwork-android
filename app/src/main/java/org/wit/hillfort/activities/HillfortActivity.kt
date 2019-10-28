c14e626563a9ca4d026c9b6d3b73cb861e7b2c27
Piotr Baran <bp84dev@outlook.com>
2019-10-28 17:25:38 +0000

    Added layout for HillfortActivity

--- app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
@@ -1,7 +1,9 @@
 package org.wit.hillfort.activities
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_hillfort.*
 import org.wit.hillfort.R
 
 class HillfortActivity : AppCompatActivity() {
@@ -9,5 +11,13 @@ class HillfortActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort)
+        // enable the action bar (toolbar title defaults to the application name)
+        setSupportActionBar(toolbar_editHillfort)
+    }
+
+    // load the menu resource (inflate the menu)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_hillfort, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
